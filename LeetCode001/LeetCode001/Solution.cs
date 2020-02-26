4b4c0ab7fddf2c06102fbd8ea9568bfaf34e665c
Borys Sokolov <Borys.Sokolov@infopulse.com>
2020-02-26 13:22:00 +0200

    LeetCode 001

---
+++ LeetCode001/LeetCode001/Solution.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode001
+{
+    public class Solution
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            var sum = new Dictionary<int, int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (sum.ContainsKey(nums[i]))
+                {
+                    return new int[] { sum[nums[i]], i };
+                }
+
+                sum[target - nums[i]] = i;
+            }
+
+            return null;
+        }
+    }
+}
