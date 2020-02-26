4dd01758872867dc835e605e00952705e53830d7
Borys Sokolov <Borys.Sokolov@infopulse.com>
2020-02-26 12:46:17 +0200

    LeetCode 33

---
+++ LeetCode033/LeetCode033/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace LeetCode033
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var nums = new int[] { 4, 5, 6, 7, 0, 1, 2 };
+            Solution solution = new Solution();
+            
+            //Console.WriteLine(solution.Search(nums, 4));
+            //Console.WriteLine(solution.Search(nums, 3));
+            //Console.WriteLine(solution.Search(nums, 5));
+
+            var nums2 = new int[] { 9, 0, 2, 7, 8 };
+            Console.WriteLine(solution.Search(nums2, 3));
+        }
+    }
+}
---
+++ LeetCode033/LeetCode033/Solution.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace LeetCode033
+{
+    public class Solution
+    {
+        public int Search(int[] nums, int target)
+        {
+            if (nums.Length < 1)
+                return -1;
+
+            if (nums[0] == target)
+                return 0;
+
+            if (nums.Length <= 4)
+            {
+                for (int i = 0; i < nums.Length; i++)
+                {
+                    if (nums[i] == target)
+                        return i;
+                }
+            }
+            else if (nums[0] < target)
+            {
+                for (int i = Math.Min(target - nums[0], nums.Length - 1); i >= 1; i--)
+                {
+                    if (nums[i] == target)
+                        return i;
+                }
+            }
+            else 
+            {
+                for (int i = Math.Min(nums.Length - nums[nums.Length-1] + target - 1, nums.Length - 1); i < nums.Length; i++)
+                {
+
+                    if (nums[i] == target)
+                        return i;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
