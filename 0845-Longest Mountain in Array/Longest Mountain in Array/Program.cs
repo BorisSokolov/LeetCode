b03cf02aae396e58f3c390d86f346302875fa560
Borys Sokolov <Borys.Sokolov@infopulse.com>
2020-11-17 20:15:35 +0200

    Changing the folder structure Adding the Longes Mountain problem

---
+++ 0845-Longest Mountain in Array/Longest Mountain in Array/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Longest_Mountain_in_Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution s = new Solution();
+            //Input: [2,1,4,7,3,2,5]
+            //Output: 5
+            //Explanation: The largest mountain is [1, 4, 7, 3, 2] which has length 5.
+            var a = new int[] { 2, 1, 4, 7, 3, 2, 5 };
+            Console.WriteLine(s.LongestMountain(a));
+
+            //Input: [2,2,2]
+            //Output: 0
+            //Explanation: There is no mountain.
+            var a2 = new int[] { 2, 2, 2 };
+            Console.WriteLine(s.LongestMountain(a2));
+        }
+    }
+}
---
+++ 0845-Longest Mountain in Array/Longest Mountain in Array/Solution.cs
@@ -0,0 +1,47 @@
+﻿namespace Longest_Mountain_in_Array
+{
+    public class Solution
+    {
+        public int LongestMountain(int[] a)
+        {
+            if (a.Length < 3)
+                return 0;
+
+            //starting index of sub-array
+            int start = -1;
+            //ending index of sub-array
+            int end = -1;
+            //total size of the longest sub-array
+            int size = 0;
+
+            for (int i = 0; i < a.Length - 1; i++)
+            {
+                if (a[i + 1] > a[i])
+                {
+                    //new mountain
+                    if (end != -1)
+                    {
+                        end = -1;
+                        start = i;
+                    }
+
+                    if (start == -1)
+                        start = i;
+                }
+                else if (a[i + 1] < a[i] && start != -1)
+                {
+                    end = i + 1;
+                }
+                else if (a[i] == a[i + 1])
+                {
+                    start = -1;
+                    end = -1;
+                }
+
+                if (start != -1 && end != -1 && size < end - start + 1)
+                    size = end - start + 1;
+            }
+            return size;
+        }
+    }
+}
