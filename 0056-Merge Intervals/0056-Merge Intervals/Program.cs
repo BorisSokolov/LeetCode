bcbc5379c109f67737cea5354d5b2a40672084fd
Borys Sokolov <Borys.Sokolov@infopulse.com>
2020-11-19 11:12:12 +0200

    Merge Intervals problem

---
+++ 0056-Merge Intervals/0056-Merge Intervals/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Merge_Intervals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution s = new Solution();
+            //Input: intervals = [[1, 3],[2,6],[8,10],[15,18]]
+            //Output: [[1,6],[8,10],[15,18]]
+            var intervals = new int[][] { new int[]{ 1, 3 }, new int[] { 2, 6 }, new int[] { 8, 10 }, new int[] { 15, 18 } };
+            foreach(var a in s.Merge(intervals))
+            Console.WriteLine($"{a[0]} {a[1]}");
+        }
+    }
+}
---
+++ 0056-Merge Intervals/0056-Merge Intervals/Solution.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Merge_Intervals
+{
+    /// <summary>
+    /// Given an array of intervals where intervals[i] = [starti, endi], merge all overlapping intervals, and return an array of the non-overlapping intervals that cover all the intervals in the input.
+    /// </summary>
+    public class Solution
+    {
+        public int[][] Merge(int[][] intervals)
+        {
+            if (intervals.Length <= 1)
+                return intervals;
+
+            Array.Sort(intervals, new IntervalComparer());
+            
+            List<int[]> result = new List<int[]>
+            {
+                intervals[0]
+            };
+
+            for (int i = 1; i < intervals.Length; i++)
+            {
+                if (result[result.Count - 1][1] < intervals[i][0])
+                    result.Add(intervals[i]);
+                else if (result[result.Count - 1][1] < intervals[i][1])
+                    result[result.Count - 1][1] = intervals[i][1];
+            }
+
+            return result.ToArray();
+        }
+    }
+
+    public class IntervalComparer : IComparer<int[]>
+    {
+        public int Compare(int[] lhs, int[] rhs)
+        {
+            return lhs[0] == rhs[0] ? lhs[1].CompareTo(rhs[1]) : lhs[0].CompareTo(rhs[0]);
+        }
+    }
+}
