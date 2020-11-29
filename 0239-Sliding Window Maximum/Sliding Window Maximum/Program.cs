bc6fb4b44c63adc044cb1681366a795d3d62f708
Borys Sokolov <Borys.Sokolov@infopulse.com>
2020-11-29 13:32:26 +0200

    239

---
+++ 0239-Sliding Window Maximum/Sliding Window Maximum/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Sliding_Window_Maximum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution s = new Solution();
+            //Input: nums = [1, 3, -1, -3, 5, 3, 6, 7], k = 3
+            //Output: [3, 3, 5, 5, 6, 7]
+            //Explanation:
+            //Window position                Max
+            // -------------- -             -----
+            //[1  3 - 1] - 3  5  3  6  7     3
+            // 1[3 - 1 - 3] 5  3  6  7       3
+            // 1  3[-1 - 3  5] 3  6  7       5
+            // 1  3 - 1[-3  5  3] 6  7       5
+            // 1  3 - 1 - 3[5  3  6] 7       6
+            // 1  3 - 1 - 3  5[3  6  7]      7
+            Console.WriteLine($"{string.Join(", ", s.MaxSlidingWindow(new[] { 1, 3, -1, -3, 5, 3, 6, 7 }, 3))}");
+            //Input: nums = [1], k = 1
+            //Output: [1]
+            Console.WriteLine($"{string.Join(", ", s.MaxSlidingWindow(new[] { 1 }, 1))}");
+        }
+    }
+}
---
+++ 0239-Sliding Window Maximum/Sliding Window Maximum/Solution.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Sliding_Window_Maximum
+{
+    /// <summary>
+    /// You are given an array of integers nums, there is a sliding window of size k which is moving from the very left of the array to the very right. You can only see the k numbers in the window. Each time the sliding window moves right by one position.
+    /// Return the max sliding window.
+    /// </summary>
+    public class Solution
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="nums">Array of integers</param>
+        /// <param name="k">Sliding window size</param>
+        /// <returns></returns>
+        public int[] MaxSlidingWindow(int[] nums, int k)
+        {
+            int n = nums.Length;
+
+            if (n * k == 0)
+                return new int[0];
+
+            if (k == 1)
+                return nums;
+
+            int[] left = new int[n];
+            left[0] = nums[0];
+            int[] right = new int[n];
+            right[n - 1] = nums[n - 1];
+
+            for (int i = 1; i < n; i++)
+            {
+                // from left to right
+                if (i % k == 0)
+                    left[i] = nums[i];  // block_start
+                else
+                    left[i] = Math.Max(left[i - 1], nums[i]);
+
+                // from right to left
+                int j = n - i - 1;
+                if ((j + 1) % k == 0)
+                    right[j] = nums[j];  // block_end
+                else
+                    right[j] = Math.Max(right[j + 1], nums[j]);
+            }
+
+            int[] output = new int[n - k + 1];
+            for (int i = 0; i < n - k + 1; i++)
+                output[i] = Math.Max(left[i + k - 1], right[i]);
+
+            return output;
+        }
+    }
+}
