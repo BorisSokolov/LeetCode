edb7c10f1f604baf3c1ac20e5ab472dd03904312
Boris Sokolov <boris.sokolov@gmail.com>
2020-11-30 10:11:49 +0200

    485 and 938

---
+++ 0938-Range Sum of BST/Range Sum of BST/Solution.cs
@@ -0,0 +1,30 @@
+﻿namespace Range_Sum_of_BST
+{
+    public class Solution
+    {
+        public int RangeSumBST(TreeNode root, int low, int high)
+        {
+            int sum = 0;
+
+            if (root == null)
+                return 0;
+
+            if (root.val >= low && root.val <= high)
+            {
+                sum += root.val;
+                sum += RangeSumBST(root.left, low, high);
+                sum += RangeSumBST(root.right, low, high);
+            }
+            else if (root.val < low)
+            {
+                sum += RangeSumBST(root.right, low, high);
+            }
+            else if (root.val > high)
+            {
+                sum += RangeSumBST(root.left, low, high);
+            }
+
+            return sum;
+        }
+    }
+}
