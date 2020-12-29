180be30e6d71d828a4bdb57f039a2ea22eba2daf
Borys Sokolov <Borys.Sokolov@infopulse.com>
2020-12-29 11:00:37 +0200

    0754

---
+++ 0754-Reach a Number/Reach a Number/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Reach_a_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution s = new Solution();
+            //Input: target = 3
+            //Output: 2
+            //Explanation:
+            //            On the first move we step from 0 to 1.
+            //On the second step we step from 1 to 3.
+            Console.WriteLine(s.ReachNumber(3));
+        }
+    }
+}
---
+++ 0754-Reach a Number/Reach a Number/Solution.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Reach_a_Number
+{
+    /// <summary>
+    /// You are standing at position 0 on an infinite number line. There is a goal at position target.
+    /// On each move, you can either go left or right.During the n-th move(starting from 1), you take n steps.
+    /// Return the minimum number of steps required to reach the destination.
+    /// </summary>
+    public class Solution
+    {
+        public int ReachNumber(int target)
+        {
+            target = Math.Abs(target);
+
+            int k = 0;
+            while (target > 0)
+                target -= ++k;
+
+            return target % 2 == 0 ? k : k + 1 + k % 2;
+        }
+
+        // O(1) solution to retest and fix
+        //public int ReachNumber(int target)
+        //{
+        //    target = Math.Abs(target);
+
+        //    int n = (int)Math.Ceiling(Math.Sqrt(8 * target + 1) / 2);
+        //    int d = n * (n + 1) / 2 - target;
+        //    n++;
+
+        //    if (d % 2 == 0)
+        //        return d;
+        //    else if ((d + n) % 2 == 0)
+        //        return n;
+        //    else
+        //        return n + 1;
+        //}
+    }
+}
