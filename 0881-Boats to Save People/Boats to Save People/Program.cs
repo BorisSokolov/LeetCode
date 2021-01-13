49a8d2bd688fbbd8f7739ca61a3e69b891d1970a
Borys Sokolov <Borys.Sokolov@infopulse.com>
2021-01-13 18:36:43 +0200

    881 and adding Utils

---
+++ 0881-Boats to Save People/Boats to Save People/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Boats_to_Save_People
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution s = new Solution();
+            //Input: people = [1, 2], limit = 3
+            //Output: 1
+            //Explanation: 1 boat(1, 2)
+            Console.WriteLine(s.NumRescueBoats(new[] { 1, 2 }, 3));
+            //Input: people = [3, 2, 2, 1], limit = 3
+            //Output: 3
+            //Explanation: 3 boats(1, 2), (2) and(3)
+            Console.WriteLine(s.NumRescueBoats(new[] { 3, 2, 2, 1 }, 3));
+            //Input: people = [3, 5, 3, 4], limit = 5
+            //Output: 4
+            //Explanation: 4 boats(3), (3), (4), (5)
+            Console.WriteLine(s.NumRescueBoats(new[] { 3, 5, 3, 4 }, 5));
+        }
+    }
+}
---
+++ 0881-Boats to Save People/Boats to Save People/Solution.cs
@@ -0,0 +1,38 @@
+﻿using LeetCode.Utils;
+namespace Boats_to_Save_People
+{
+    /// <summary>
+    /// The i-th person has weight people[i], and each boat can carry a maximum weight of limit.
+    /// Each boat carries at most 2 people at the same time, provided the sum of the weight of those people is at most limit.
+    /// Return the minimum number of boats to carry every given person.  (It is guaranteed each person can be carried by a boat.)
+    /// </summary>
+    public class Solution
+    {
+        public int NumRescueBoats(int[] people, int limit)
+        {
+            if (people == null || people.Length < 1)
+                return 0;
+
+            //Two pointer approach as each boat can handle at most two people at the same time
+            //Changing Array.Sort that uses a Quicksort to more efficient Radix sort
+            //Array.Sort(people);
+            Sorting.RadixSort(people);
+
+            int start = 0;
+            int end = people.Length - 1;
+            int boatCount = 0;
+
+            while (start <= end)
+            {
+                //check if the boat can handle two 
+                if (people[start] + people[end] <= limit)
+                    start++;
+
+                end--;
+                boatCount++;
+            }
+
+            return boatCount;
+        }
+    }
+}
