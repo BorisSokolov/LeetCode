bdf70140c8a572193ca67c7e02d95160965df3b1
Borys Sokolov <Borys.Sokolov@infopulse.com>
2021-01-13 11:07:30 +0200

    88

---
+++ 0088-Merge Sorted Array/Merge Sorted Array/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Merge_Sorted_Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution s = new Solution();
+            //Input: nums1 = [1, 2, 3, 0, 0, 0], m = 3, nums2 = [2, 5, 6], n = 3
+            //Output: [1, 2, 2, 3, 5, 6]
+            var a = new[] { 1, 2, 3, 0, 0, 0 };
+            var b = new[] { 2, 5, 6 };
+            s.Merge(a, 3, b, 3);
+            Console.WriteLine(string.Join(", ", a));
+            //Input: nums1 = [1], m = 1, nums2 = [], n = 0
+            //Output: [1]
+            a = new[] { 1,};
+            b = new int[] { };
+            s.Merge(a, 1, b, 0);
+            Console.WriteLine(string.Join(", ", a));
+        }
+    }
+}
---
+++ 0088-Merge Sorted Array/Merge Sorted Array/Solution.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Merge_Sorted_Array
+{
+    /// <summary>
+    /// Given two sorted integer arrays nums1 and nums2, merge nums2 into nums1 as one sorted array.
+    /// The number of elements initialized in nums1 and nums2 are m and n respectively.You may assume that nums1 has enough space (size that is equal to m + n) to hold additional elements from nums2.
+    /// </summary>
+    public class Solution
+    {
+        public void Merge(int[] nums1, int m, int[] nums2, int n)
+        {
+            // two get pointers for nums1 and nums2
+            int p1 = m - 1;
+            int p2 = n - 1;
+            // set pointer for nums1
+            int p = m + n - 1;
+
+            // while there are still elements to compare
+            while ((p1 >= 0) && (p2 >= 0))
+                // compare two elements from nums1 and nums2 
+                // and add the largest one in nums1 
+                nums1[p--] = (nums1[p1] < nums2[p2]) ? nums2[p2--] : nums1[p1--];
+
+            // add missing elements from nums2
+            Array.Copy(nums2, 0, nums1, 0, p2 + 1);
+        }
+    }
+}
